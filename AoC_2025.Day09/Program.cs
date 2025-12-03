8818b2efe4c522036180367c8f40fc6be42f4ead
markus-bo <markus.bodlos@outlook.com>
2025-12-03 07:24:10 +0000

    Add templates for Day 4-12

---
+++ AoC_2025.Day09/Program.cs
@@ -0,0 +1,32 @@
+﻿using AoC_Toolbox;
+using AoC_Toolbox.Geometry;
+using AoC_Toolbox.InputParsing;
+using AoC_Toolbox.Pathfinding;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Diagnostics;
+
+internal class Solution
+{
+    static void Main(string[] args)
+    {
+        PuzzleSetup.Solve(solutionPart1, solutionPart2);
+    }
+
+    static object? solutionPart1(string[] input)
+    {
+        var result = 0;
+
+        return result;
+    }
+
+    static object? solutionPart2(string[] input)
+    {
+        var result = 0;
+
+        return result;
+    }
+}
