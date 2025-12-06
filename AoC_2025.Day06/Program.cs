9423a2877deffbbb07efd4317242c865ab1fb75f
markus-bo <markus.bodlos@outlook.com>
2025-12-06 09:56:29 +0000

    Add Solution Day 6

--- AoC_2025.Day06/Program.cs
+++ AoC_2025.Day06/Program.cs
@@ -18,14 +18,76 @@ internal class Solution
 
     static object? solutionPart1(string[] input)
     {
-        var result = 0;
+        var result = 0L;
+
+        var numbers = input.SkipLast(1).Select(
+                x => x.Split().Where(y => string.IsNullOrWhiteSpace(y) == false).Select(y => int.Parse(y.Trim())).ToList()).ToList();
+        
+
+        var operators = input.Last().Split().Where(y => string.IsNullOrWhiteSpace(y) == false).ToList();
+
+        for(int i = 0; i < numbers.First().Count(); i++)
+        {
+            var intermediateResult = operators[i] == "*" ? 1L : 0L;
+
+            for(int j = 0; j < numbers.Count; j++)
+            {
+                if (operators[i] == "*")
+                {
+                    intermediateResult *= numbers[j][i]; 
+                }
+                else if (operators[i] == "+")
+                {
+                    intermediateResult += numbers[j][i]; 
+                }
+                else
+                    throw new NotImplementedException();
+            }
+
+            result += intermediateResult;
+        }
 
         return result;
     }
 
     static object? solutionPart2(string[] input)
     {
-        var result = 0;
+        var result = 0L;
+
+        var nums = new List<long>();
+
+        for (int i = input.First().Length - 1; i >= 0 ; i--)
+        {
+            var numString = "";
+
+            for (int j = 0; j < input.Length - 1; j++)
+            {
+                numString += input[j][i];
+            }
+            
+            if (string.IsNullOrWhiteSpace(numString))
+                continue;
+
+            var num = long.Parse(numString);
+
+            nums.Add(num);
+
+            if (input[input.Length - 1][i] == ' ')
+                continue;
+
+            if (input[input.Length - 1][i] == '+')
+            {
+                result += nums.Sum();
+                nums.Clear();
+            }
+            else if (input[input.Length - 1][i] == '*')
+            {
+                result += nums.Aggregate(1L, (a,b) => a*b);
+                nums.Clear();
+            }
+            else
+                throw new NotImplementedException(); 
+        }
 
         return result;
     }
