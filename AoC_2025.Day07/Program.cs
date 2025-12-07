66a7c69998aaa4392affb8aa1945d332c64df276
markus-bo <markus.bodlos@outlook.com>
2025-12-07 19:14:03 +0000

    Add Solution Day 7

--- AoC_2025.Day07/Program.cs
+++ AoC_2025.Day07/Program.cs
@@ -20,13 +20,79 @@ internal class Solution
     {
         var result = 0;
 
+        var line = new bool[input.First().Length];
+
+        var xStart = input.First().IndexOf('S');
+        line[xStart] = true;
+
+        for (int i = 1; i < input.Length; i++)
+        {
+            for (int j = 0; j < input[i].Length; j++)
+            {
+                if (input[i][j] == '^' && line[j] == true) // split
+                {
+                    result++;
+
+                    line[j] = false;
+                    
+                    if ((j - 1) >= 0)
+                        line[j-1] = true;
+                    
+                    if ((j+1) < line.Length)
+                        line[j+1] = true;
+                }        
+            }   
+        }
         return result;
     }
 
     static object? solutionPart2(string[] input)
     {
-        var result = 0;
+        var xStart = input.First().IndexOf('S');
+
+        map = input;
+        memo.Clear();
+
+        var result = dfs(xStart, 1);
 
         return result;
+    }
+
+    static string[] map;
+
+    static Dictionary<(int y, int x), long> memo = new Dictionary<(int y, int x), long>();
+
+    static long dfs(int xCurrent, int yCurrent)
+    {
+        if (memo.ContainsKey((yCurrent, xCurrent)))
+        {
+            return memo[(yCurrent, xCurrent)];
+        }
+
+        if (yCurrent == map.Length)
+        {
+            return 1L;
+        }
+
+        if (xCurrent < 0 || xCurrent >= map.First().Length)
+        {
+            return 0L;
+        }
+
+        var _count = 0L;
+
+        if (map[yCurrent][xCurrent] == '^')
+        {
+            _count += dfs(xCurrent - 1, yCurrent + 1);
+            _count += dfs(xCurrent + 1, yCurrent + 1);
+        }
+        else
+        {
+            _count += dfs(xCurrent, yCurrent + 1);
+        }
+
+        memo.Add((yCurrent, xCurrent), _count);
+
+        return _count;
     }
 }
